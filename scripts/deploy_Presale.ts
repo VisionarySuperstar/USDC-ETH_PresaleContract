2db95eae9b4a6e167e621272e2df03eb505e9f76
Superstar-lucas <lucaslee971127@gmail.com>
2024-07-05 03:02:04 +0800

    fixed some logic

--- scripts/deploy_Presale.ts
+++ scripts/deploy_Presale.ts
@@ -5,9 +5,7 @@ import { ethers } from 'hardhat'
 async function main() {
     // Retrieve the first signer, typically the default account in Hardhat, to use as the deployer.
     const [deployer] = await ethers.getSigners();
-    const endTime = Math.floor(Date.now() / 1000) + 10000;
-    console.log("endTime", endTime);
-    const instancePresale = await ethers.deployContract("Presale", [endTime]);
+    const instancePresale = await ethers.deployContract("Presale");
     await instancePresale.waitForDeployment()
     const Presale_Address = await instancePresale.getAddress();
     console.log(`Presale is deployed. ${Presale_Address}`); //
